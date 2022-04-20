9f70fc992b962bb1d8294db287efbacdd0ecead2
seungchae <s2125@e-mirim.hs.kr>
2022-04-21 08:49:58 +0900

    구구단(옆으로 출력)

---
+++ Codingtest/gugudan.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	for (int i = 1; i <=9; i++) {
+		for (int j = 2; j <=9; j++) {
+			printf("%d X %d = %d\t", j, i, j * i);
+		}
+		printf("\n");
+	}
+	return 0;
+}
