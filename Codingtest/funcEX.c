bbc2487b7b867d1621d1954d444917f31442b2a5
seungchae <s2125@e-mirim.hs.kr>
2022-05-12 10:14:27 +0900

    짝/홀 판별하는 함수

--- Codingtest/funcEX.c
+++ Codingtest/funcEX.c
@@ -8,9 +8,13 @@ void display(int length, char ch) {
 }
 
 //짝수인지 홀수인지 판별, 키보드 입력x
+int isEven(int n) {
+	return (n % 2 == 0 ? 1 : 0);
+}
 
 int main() {
 	display(20, '#');
-
+	if (isEven(11)) printf("\n짝수입니다.");
+	else printf("\n홀수입니다.");
 	return 0;
 }
