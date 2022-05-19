62e1be795d5e884bb55ac4ee3703f1a5d85338e0
seungchae <s2125@e-mirim.hs.kr>
2022-05-19 09:13:32 +0900

    swap(pointer) 전후비교

--- Codingtest/valueSwap.c
+++ Codingtest/valueSwap.c
@@ -10,7 +10,11 @@ void swap(int* a, int* b)
 int main()
 {
 	int a = 5, b = 8;
+	printf("함수 호출 전 : \n");
+	printf("a : %d b : %d\n", a, b);
+
 	swap(&a, &b);
+
 	printf("함수 호출 후 : \n");
 	printf("a : %d b : %d\n", a, b);
 }
